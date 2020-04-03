75fe86433c73b9220cfaa3438b4ce8f9179757fb
Ana Sofia <anasalgadomontoya@gmail.com>
2020-04-03 15:40:19 -0700

    Proyecto

---
+++ app/src/main/java/salgadomontoya/anasofia/myreduce/Detalle_articulo.kt
@@ -0,0 +1,12 @@
+package salgadomontoya.anasofia.myreduce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Detalle_articulo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_articulo)
+    }
+}
---
+++ app/src/main/java/salgadomontoya/anasofia/myreduce/HomeBlog.kt
@@ -0,0 +1,12 @@
+package salgadomontoya.anasofia.myreduce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeBlog : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_blog)
+    }
+}
