8424c20bd9654b82aab3a6c0e85bfd85adc61d62
Ana Sofia <anasalgadomontoya@gmail.com>
2020-04-22 21:02:09 -0700

    LogIn añadido

--- app/src/main/java/salgadomontoya/anasofia/myreduce/Principal_Activity.kt
+++ app/src/main/java/salgadomontoya/anasofia/myreduce/Principal_Activity.kt
@@ -2,11 +2,24 @@ package salgadomontoya.anasofia.myreduce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
 class Principal_Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal_sesion)
+
+
+        val bundle= intent.extras
+        if(bundle != null){
+            val nombre= bundle.getString("name")
+            val correo= bundle.getString("email")
+
+        }
+
+
+
+
     }
 }
