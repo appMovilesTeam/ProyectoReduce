cf577187af42e25c1f4c1801f021858a0a503036
Ana Sofia <anasalgadomontoya@gmail.com>
2020-05-07 12:43:37 -0700

    Se agrego la pantalla de agregar actividad

---
+++ app/src/main/java/salgadomontoya/anasofia/myreduce/AgregarActividad.kt
@@ -0,0 +1,12 @@
+package salgadomontoya.anasofia.myreduce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AgregarActividad : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_actividad)
+    }
+}
