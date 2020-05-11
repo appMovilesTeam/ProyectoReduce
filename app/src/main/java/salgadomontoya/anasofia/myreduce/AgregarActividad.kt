22ca89fa0ddee466200efd7135c2af5cca5517d2
Luis García <lgarcia174099@alumno.itson.edu.mx>
2020-05-11 00:29:06 -0700

    update article layout

--- app/src/main/java/salgadomontoya/anasofia/myreduce/AgregarActividad.kt
+++ app/src/main/java/salgadomontoya/anasofia/myreduce/AgregarActividad.kt
@@ -3,6 +3,9 @@ package salgadomontoya.anasofia.myreduce
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.Button
 import android.widget.ImageButton
 
@@ -12,12 +15,33 @@ class AgregarActividad : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agregar_actividad)
 
-        val buttonAtras : ImageButton=findViewById(R.id.regresar_atras) as ImageButton
+        val buttonAtras : ImageButton= findViewById<ImageButton>(R.id.regresar_atras)
+        //val arrayArticulo : ArrayList<EntradaBlog> = findViewById(R.id.)
 
         buttonAtras.setOnClickListener {
             var intent: Intent = Intent(this, PaginaPrincipal::class.java)
             startActivity(intent)
         }
 
+    }
+
+    class AdapterActividad: BaseAdapter {
+        constructor()
+
+        override fun getItem(position: Int): Any {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemId(position: Int): Long {
+            TODO("Not yet implemented")
+        }
+
+        override fun getCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            TODO("Not yet implemented")
+        }
     }
 }
